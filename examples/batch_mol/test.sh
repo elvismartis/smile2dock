7bf5af51851660922f60af1f99165854980e55e6
Elvis Martis <8048307+elvismartis@users.noreply.github.com>
2025-06-19 17:44:42 +0200

    tmp (#6)

--- examples/batch_mol/test.sh
+++ examples/batch_mol/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "This is a test script to be used as tutorial"
 echo "Convert a single smile to 3D molecular formats ready for docking"
+
 python3 ../../smile2dock.py -i test.smi -o ./ --reference "CCC(=O)CC(O)C" --protonate --ph_min 1.0 --ph_max 7.4 > test.log
+
 
--- obsolete_files/smile2dock.py
+++ obsolete_files/smile2dock.py
@@ -4,6 +4,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem, Descriptors, DataStructs, rdMolDescriptors, Crippen
 from rdkit.Chem.Fingerprints import FingerprintMols
 from openbabel import pybel
+
 import dimorphite_dl
 
 def protonate_smiles(smiles, ph_min=6.4, ph_max=8.4, precision=1.0, max_variants=128):
